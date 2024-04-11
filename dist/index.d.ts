15d14ce0ba335db9baab4ac3afb6a12569093ad5
Alfon Labadan <alfonlabadan@gmail.com>
2024-04-11 16:21:36 +0800

    initial update

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,1 @@
 export * from "./createStore";
-export * from "./middleware/broadcast";
-export * from "./middleware/hydrateState";
-export * from "./middleware/logging";
-export * from "./middleware/preserve";
---
+++ src/middleware/index.js
@@ -0,0 +1,4 @@
+export * from "./broadcast";
+export * from "./hydrateState";
+export * from "./logging";
+export * from "./preserve";
